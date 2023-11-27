b28b452f9c7f23b5194b9c259d873522386eb9a8
Diana Castaño <dianaczapata@gmail.com>
2023-11-27 04:52:20 -0500

    added details of El nido del paisita project

--- src/proyecto-montañerito-paisa.jsx
+++ src/proyecto-montañerito-paisa.jsx
@@ -1,8 +1,153 @@
 import React from "react"
+import "./Proyecto-design.css"
 
 const Paisita= () =>{
  return <>
-  <h1>este es el proyecto sobre el montañerito paisa</h1>
+ <article className="project-body">
+  <h1>Web app El nido del paisita</h1>
+  <div className="section-part">
+     <h2>Objetivo del proyecto</h2>
+   <div className="section-info">
+    <p>
+     El nido del paisita es una organización con sede en Antioquia (Colombia) cuyo enfoque central de trabajo es la protección del ave el montañerito paisa (<span className="name-bird">Atlapetes blancae</span>). La organización necesita una herramienta que ayude a las personas realizar donativos para la protección de este gorrión.
+    </p>
+    <img src="/nido-del-paisita.png" alt="home-page del nido del paisita" />
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Mi rol del proyecto</h2>
+   <div className="section-info">
+    <img src="/imagen-diseñadora.jpg" alt="imagen deuna diseñadora" className="img-rol"/>
+    <p>
+     Mi función consistió en ser la diseñadora de UX que lidera el diseño del web app El nido del paisita. Mis responsabilidades consistieron en realizar entrevistas y esquemas en papel y digitales; crear prototipos de baja y alta fidelidad; realizar estudios de facilidad de uso; tener en cuenta la accesibilidad; repetir los diseños y el diseño adaptable.
+    </p>
+   </div>
+  </div>
+   <div className="section-part">
+   <h2>Objetivo del proyecto</h2>
+   <div className="section-info">
+    <p>
+     Diseñar una aplicación que promueva la donación, que serviran como fondos para la conservación y la educación sobre el gorrión montañerito paisa, y que ayude a las personas a crear conciencia sobre el estado actual de la fauna y flora nativa del país.  
+    </p>
+    <img src="/objetivos.jpg" alt="inagen de objetivos" />
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Público objetivo</h2>
+   <div className="section-info">
+    <img src="/publico-objetivo.jpg" alt="imagen público objetivo" className="img-rol"/>
+    <p>
+      Los principales usuarios objetivo de El nido del paisita son los estudiantes universitarios y adultos que están preocupados por el medio ambiente y la conservasión de especies, y a quienes les gustaría aprender más sobre lo que pueden hacer para proteger esta especie en peligro de extinción. 
+    </p>
+   </div>
+  </div>
+   <div className="section-part">
+   <h2>Detalles del estudio de investigación</h2>
+   <div className="section-info change-position">
+    <p>
+     Usé los datos de El nido del paisita sobre las ayudas para proteger a un animal en peligro de extinción, para desarrollar  las preguntas, que luego se utilizaron para realizar entrevistas a los usuarios. La mayoría de los participantes de la entrevista informaron sentirse mal por la posible desaparición de una especie única en Antioquia, pero no reaccionaron de manera activa para ayudar a proteger a esta ave de terminar extinta. 
+    </p>
+    <br />
+    <p>
+     Los comentarios recibidos durante la investigación dejaron muy claro que los usuarios estarían abiertos y dispuestos a esforzarse por  apoyar en la protección de este gorrión, si tuvieran acceso a una herramienta fácil de usar como guía.
+    </p>
+    <img src="/investigacion.jpg" alt="imagen de investigación" />
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Los conceptos de diseño iniciales</h2>
+   <div className="section-info">
+    <p>
+     Hice un ejercicio de ideación rápido para tener ideas sobre cómo abordar las brechas identificadas en la auditoría competitiva. Mi enfoque se centró específicamente en la acción rápida de hacer donaciones, para conformar los fondos para proteger al montañerito paisa .
+    </p>
+    <img src="/boceto-montañerito.png" alt="boceto de papel" />
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Bocetos o esquemas de página</h2>
+   <div className="section-info">
+    <img src="/esquema-digital-pasteleria.png" alt="imagen deuna diseñadora" className="img-rol"/>
+    <div>
+     <p>
+     Después de idear y crear una versión preliminar de algunos esquema de página en papel, creé los diseños iniciales para la aplicación el nido del paisita. Estos diseños se centraron en ofrecer orientación personalizada a los usuarios para ayudarlos a las donaciones de manera fácil. 
+     </p>
+     <br />
+     <p>Se pueden ver más detalles del esquema de página, a través del prototipo de baja fidelidad haciendo click <a href="https://www.figma.com/proto/DcaQgMjSQ3UfmiXWedoG7f/App-montañerito-paisa?type=design&node-id=1-2&t=WsVcqVlbUJj8NtGA-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2&mode=design" className="lo-fi-link">aquí</a> </p>
+    </div>
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Resultados de las pruebas de usuario</h2>
+   <div className="section-info estudio-resultados">
+    <div className="resultados-card">
+     <h3>
+      Donación
+     </h3>
+      <p>
+     La gente quiere tener la opción de ofrecer diferentes cantidades de donación, además de las opciones predeterminadas 
+    </p>
+    </div>
+    <div className="resultados-card">
+      <h3>
+       Añadir otras opciones de pago
+      </h3>
+      <p>
+      Desean que estén otras opciones de pago además de la opción de tarjeta de crédito
+      </p>
+    </div>
+    <div className="resultados-card">
+      <h3>
+       Confirmación de la donación
+      </h3>
+      <p>
+       La gente prefería contar con indicaciones claras para realizar la donación exitosamente
+      </p>
+    </div>
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Maquetas o prototipos de alta fidelidad de diseños finales y pulidos</h2>
+   <div className="section-info">
+    <img src="/hi-fi-nido-paisita.png" alt="imagen deuna diseñadora" className="img-rol"/>
+    <div>
+     <p>
+      Con base en las conclusiones de los estudios de usabilidad, apliqué cambios de diseño como proporcionar una secuencia clara para realizar la donación exitosamente.
+     </p>
+     <br />
+     <p>Se pueden ver más detalles del esquema de página, a través del prototipo de alta fidelidad haciendo click <a href="https://www.figma.com/proto/DcaQgMjSQ3UfmiXWedoG7f/App-montañerito-paisa?type=design&node-id=74-155&t=bqpjCKQ860MZ40Wj-1&scaling=scale-down&page-id=30%3A2&starting-point-node-id=30%3A3&mode=design" target="_blank" rel="noopener noreferrer" className="lo-fi-link">aquí</a> </p>
+    </div>
+   </div>
+  </div>
+  <div className="section-part">
+   <h2>Conclusión y los posibles pasos por seguir</h2>
+   <div className="section-info estudio-resultados">
+    <div className="resultados-card">
+     <h3>
+      Conclusión
+     </h3>
+      <p>
+     Los usuarios comentaron que la aplicación hizo que el proceso de donación para ayudar al montañerito paisa sea fácil de hacer.
+    </p>
+    </div>
+    <div className="resultados-card">
+      <h3>
+       1- Próximo paso
+      </h3>
+      <p>
+     Realizar investigaciones sobre el alcance de la aplicación para ayudar a proteger al montañerito paisa.
+      </p>
+    </div>
+    <div className="resultados-card">
+      <h3>
+       2- Próximo paso
+      </h3>
+      <p>
+      Agregar más recursos educativos para que los usuarios aprendan sobre el habitat del montañerito paisa.
+      </p>
+    </div>
+   </div>
+  </div>
+ </article>
  </>
 }
 
