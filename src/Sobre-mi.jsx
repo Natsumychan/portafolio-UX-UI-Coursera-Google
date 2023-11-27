fa44209e4ee604eb0e150e3189bfb3c6b219d25a
Diana Castaño <dianaczapata@gmail.com>
2023-11-27 05:01:22 -0500

    update info sobre mi

--- src/Sobre-mi.jsx
+++ src/Sobre-mi.jsx
@@ -9,7 +9,7 @@ const SobreMi = () =>{
    </p>
    <br/>
    <p>
-     Durante mis estudios he podido empezar mi carrera como frontend developer desde cero, aprendiendo las bases de la programación y sobre herramientas básicas como HTML, CSS y JavaScript para empezar a construir mi portafolio. Llevo poco tiempo en esta formación, pero tengo el enfoque y la motivación para formarme como profesional y nunca parar de aprender.
+     Durante mis estudios he podido empezar mi carrera como frontend developer desde cero, aprendiendo las bases de la programación y sobre herramientas básicas como HTML, CSS y JavaScript para empezar a construir mi portafolio. También he complementado mi formación con el curso de UX y UI de Coursera y Gogle usando figma, para comprender mejor la perspectiva del diseñador, y mejorar la comunicación con ellos. Continuo con mi formaciónpara empezar a abrirme paso y optener mi primer empleo, y tengo el enfoque y la motivación para continuar por este camino y nunca parar de aprender.
    </p>
    <button className="boton-cv"><a href="/CV-Diana_Castaño.pdf" download>Descargar CV</a></button>
   </section>
