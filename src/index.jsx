75bceb6b15442441daea431f13cb797dc0572d34
Diana Castaño <dianaczapata@gmail.com>
2023-11-26 21:30:33 -0500

    hero ready

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom"
-import Button from 'react-bootstrap/Button'
+import './index.css'
 
 const Inicio= () =>{
  return <>
-  <h1>Esta es la página de inicio</h1>
+  <main>
+   <div className="hero-img">
+   </div>
+   <div className="hero-title">
+    <h1 ><span>👋</span> Hola a todos, y bienvenidos a mi sitio web <span>😃</span></h1>
+   </div>
+  </main>
  <button><a href="/CV-Diana_Castaño.pdf" download>Descargar CV</a></button>
   <Link to="peliculas">Haz click para ver el proyecto de peliculas</Link>
   <Link to="pasteles">Haz click para ver el proyecto de peliculas</Link>
